bdb0565aa0041fb75825ca4b81088369a99e2ebd
Kip Kosek <kipkosek@gmail.com>
2016-09-01 10:16:13 -0400

    Assignment 8 DOM Animation: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,16 @@
 var animatePoints = function() {
-                
-                var points = document.getElementsByClassName('point');
-                
-                var revealPoint = function() {
-                    for (var i=0; i<=2; i++) {
-                        points[i].style.opacity = 1;
-                        points[i].style.transform = "scaleX(1) translateY(0)";
-                        points[i].style.msTransform = "scaleX(1) translateY(0)";
-                        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-                    }
-                };
-                    revealPoint();
-                };
+    var points = document.getElementsByClassName('point');
+
+    var revealPoint = function(point) {
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+    
+    for (var i=0; i < points.length; i++) {
+        revealPoint(points[i]);
+    }  
+};
     
         
