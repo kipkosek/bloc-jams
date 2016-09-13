b5dc4f4cd507d001c9dcbcd6b7aa97596d3fd5ff
Kip Kosek <kipkosek@gmail.com>
2016-09-13 11:35:56 -0400

    Checkpoint 16 jQuery Landing Page: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,25 +1,24 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
-    var revealPoint = function(point) {
-        point.style.opacity = 1;
-        point.style.transform = "scaleX(1) translateY(0)";
-        point.style.msTransform = "scaleX(1) translateY(0)";
-        point.style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    
-    for (var i=0; i < points.length; i++) {
-        revealPoint(points[i]);
-    }  
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };    
+    $.each($('.point'), revealPoint);
 };
     
-window.onload = function() {
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+$(window).load(function() {
+    if ($(window).height() > 950){
+        animatePoints();
+    }
     
-    window.addEventListener("scroll", function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+    $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     });
-}        
+});
+        
