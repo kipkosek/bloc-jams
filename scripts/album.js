540c6fd8b1264e961d91bff6086a00e8bf3ff6ae
Kip Kosek <kipkosek@gmail.com>
2016-09-18 16:55:57 -0400

    Assignment 20 jQuery Buzz: Completed

--- scripts/album.js
+++ scripts/album.js
@@ -81,7 +81,7 @@ var clickHandler = function(){
                 currentSoundFile.play();
             } else {
                 $(this).html(playButtonTemplate);
-                $('.main-controls .play-pause').html(playerBarPauseButton);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
                 currentSoundFile.pause();
             }
             
@@ -173,6 +173,27 @@ var previousSong = function() {
     
 };
 
+var togglePlayFromPlayerBar = function() {
+    if (currentSoundFile.isPaused()) {
+        $(playButtonTemplate).html(pauseButtonTemplate);
+        $($playPauseButton).html(playerBarPauseButton);
+        currentSoundFile.play();
+    } else if (currentSoundFile) {
+        $(pauseButtonTemplate).html(playButtonTemplate);
+        $($playPauseButton).html(playerBarPlayButton);
+        currentSoundFile.pause();
+    }       
+};
+
+//if(currentSoundFile.isPaused()) {
+//                $(this).html(pauseButtonTemplate);
+//                $('.main-controls .play-pause').html(playerBarPauseButton);
+//                currentSoundFile.play();
+//            } else {
+//                $(this).html(playButtonTemplate);
+//                $('.main-controls .play-pause').html(playerBarPauseButton);
+//                currentSoundFile.pause();
+
 var updatePlayerBarSong = function() {
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
     $('.currently-playing .artist-name').text(currentSongFromAlbum.artist);
@@ -192,10 +213,12 @@ var currentSoundFile = null;
 var currentVolume = 80;
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playPauseButton.click(togglePlayFromPlayerBar);
 });
